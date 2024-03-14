ad5e8ebe393e7020ff7b71a47d0b601b169d3bad
sodtiruk <sodtiruk22@gmail.com>
2024-03-14 15:03:02 +0700

    learn animation

---
+++ Assets/SUGame/Scripts/ElevatorController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator elevatorAnimator;
+    public int floor = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        elevatorAnimator.SetInteger("AtFloor", floor);
+    }
+
+}
